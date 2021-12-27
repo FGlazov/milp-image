ae74fc0aa660e938e87d58908f1c869117f8b12d
Haidi Shoe <haidi-loves-shoes@shoemaniac.de>
2021-12-27 14:08:44 +0100

    Update liscense in setup.py.

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setuptools.setup(
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
+        "License :: OSI Approved :: Eclipse Public License 2.0 (EPL-2.0)",
         "Operating System :: OS Independent",
     ],
     install_requires=[
