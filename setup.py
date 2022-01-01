f133ea12b4a2de705595b362fcf878671db12ec0
Fedor Glazov <fedorglazov@gmail.com>
2022-01-01 14:25:17 +0100

    Migrate requirments and add version numbers.

--- setup.py
+++ setup.py
@@ -3,6 +3,9 @@ import setuptools
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
 setuptools.setup(
     name="milp_imgcomp",
     version="0.0.1",
@@ -13,19 +16,11 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/FGlazov/milp-image",
     packages=setuptools.find_packages(where="milp_imgcomp"),
-    package_dir={"": "milp_imgcomp"}
+    package_dir={"": "milp_imgcomp"},
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: Eclipse Public License 2.0 (EPL-2.0)",
         "Operating System :: OS Independent",
     ],
-    install_requires=[
-        'opencv-python',
-        'py_rans',
-        'pip',
-        'mip',
-        'numpy',
-        'scipy',
-        'sklearn'
-    ],
+    install_requires=required,
 )
